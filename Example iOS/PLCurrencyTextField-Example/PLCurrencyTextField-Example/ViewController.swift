bafd6c36fe2d18c9c84b69b6f6d1f0a5916b8363
Łukasz Śliwiński <sliwinski.lukas@gmail.com>
2016-07-28 23:49:43 +0200

    Fixed example. Improved README.md Added gif.

---
+++ Example iOS/PLCurrencyTextField-Example/PLCurrencyTextField-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PLCurrencyTextField-Example
+//
+//  Created by Macbook on 28/07/16.
+//  Copyright © 2016 Plum's organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
